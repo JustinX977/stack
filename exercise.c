bafeb4a31df474ea91b28e9d9c0233a51dd9c856
Justin <justinx977@gmail.com>
2025-03-22 17:01:23 +0800

    add exercise.c

---
+++ exercise.c
@@ -0,0 +1,50 @@
+#include"stack.h"
+
+/*https://leetcode.cn/problems/valid-parentheses/*/
+bool isValid(char* s) 
+{
+    stack_t st = {0};
+    stack_init(&st);
+
+    while (*s)
+    {
+        if (*s == '(' || *s == '{' || *s == '[')
+        {
+            stack_push(&st, *s);
+        }
+        else
+        {
+            datatype data = stack_top(&st);
+            stack_pop(&st);
+            if ((*s == ')' && data != '(') ||\
+                (*s == ']' && data != '[') ||\
+                (*s == '}' && data != '{'))
+            {
+                stack_destroy(&st);
+                return false;
+            }
+
+        }
+        s++;
+    }
+
+    stack_destroy(&st);
+    return true;
+}
+
+int main()
+{
+    char *s = {'(', ')', '[', ']'};
+
+    bool ret = isValid(s);
+    if (ret == true)
+    {
+        printf("It is ture\n");
+    }
+    else
+    {
+        printf("It is false\n");
+    }
+
+    return 0;
+}
